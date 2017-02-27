d6259bbefa018bf8bd9b2e4f8e47660f89d1abd1
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2017-02-27 16:47:05 -0600

    fixed button on about-user. added sample bio as an idea for a future model.

--- app/shared/services/user.service.ts
+++ app/shared/services/user.service.ts
@@ -7,7 +7,8 @@ const usersPromise: Promise<User[]> = Promise.resolve([
 		id: 1,
 		name: 'Justin',
 		username: 'Solinari',
-		avatar: 'https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-9/15781278_10154935357183760_7566106250660922199_n.jpg?oh=56673cf7a03483dda909ff2a051df313&oe=592D0075'
+		avatar: 'https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-9/15781278_10154935357183760_7566106250660922199_n.jpg?oh=56673cf7a03483dda909ff2a051df313&oe=592D0075',
+		bio: 'I am a biologist turned coder seeking new horizons as a software engineer. I am a self-driven learner and a polyglot when it comes to coding languages. I love implementing and learning to use new technologies and the alacrity by which I can master new concepts will make me an excellent addition to any team. My past experiences in the sciences and as a software engineer taught me to always wear many hats in any role I am employed in. If you would like to know more, feel free to contact me!'
 	}
 ]);
 
