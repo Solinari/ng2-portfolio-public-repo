479c1a42a87ec1591673a30394633ddc682e906d
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2017-03-06 15:16:27 -0600

    styled not-found route

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 // any route defined by user will send them here. 
 @Component({
 	// do not need selector because pushing component through router & router outlet
-	template: `
-		<div class="jumbotron text-center">
-			<h1>404 Not Found</h1>
-			<p>You are lost my friend. Turn back towards <a routerLink="/">home</a>.</p>
-		</div>
-	`
+	templateUrl: `./app/not-found/not-found.component.html`,
+	styleUrls: [`./app/not-found/not-found.component.css`]
 })
 
 export class NotFoundComponent {}
