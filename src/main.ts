6a0fb934a99b7061afb06271319155b2dc45b506
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2017-03-18 14:18:39 -0500

    reverting changes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }  from './app/app.module';
 
-import { enableProdMode } from '@angular/core';
+// import { enableProdMode } from '@angular/core';
 
-// enables prod mode unless running locally
-if (!/localhost/.test(document.location.host)) {
-	enableProdMode();
-}
+// // enables prod mode unless running locally
+// if (!/localhost/.test(document.location.host)) {
+// 	enableProdMode();
+// }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
